4a018cf77fc788145565bcf31c2de375e06482f6
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-02-06 15:19:19 +0600

    6feb2021.1

--- JavaPractice/src/App.java
+++ JavaPractice/src/App.java
@@ -1,12 +1,25 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class App {
-    public static void main(String args[]) {
-                String s1 = "hi";
-                String s2 = "               ";
-                String s3 = s1+s2;
-                int x=12;
-                //Complete this line
-                String s = String.format("%03d", x);
-                int a = Integer.parseInt(s);
-        System.out.println(s3+"     "+s);
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int re;
+        for(int i = 1;i<=10; i++){
+            re = i*N;
+            System.out.println(N+" x "+i+" = "+re);
+        }
+        scanner.close();
     }
 }
