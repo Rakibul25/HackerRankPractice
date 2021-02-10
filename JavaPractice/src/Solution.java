6f7ac3b348a12e8df4e1ac39a87fb3ab7bdd93b6
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-02-10 10:27:54 +0600

    10.02.21(EOF)

--- JavaPractice/src/Solution.java
+++ JavaPractice/src/Solution.java
@@ -1,44 +1,20 @@
-
 import java.io.*;
 import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+public class Solution {
 
-
-class Solution{
-    static String findsize(String numString)
-    {
-        String answer = "";
-        try{
-            long num = Long.parseLong(numString);
-            answer = numString + " can be fitted in:\n";
-            if((num<=Byte.MAX_VALUE) && (num>=Byte.MIN_VALUE)){
-                answer = answer.concat("* byte\n* short\n* int\n* long");
-            }else if((num <= Short.MAX_VALUE) && (num >= Short.MIN_VALUE)){
-                answer = answer.concat("* short\n* int\n* long");
-            }else if((num <= Integer.MAX_VALUE) && (num >= Integer.MIN_VALUE)){
-                    answer = answer.concat("* int\n* long");
-            }else{
-                    answer = answer.concat("* long");
-            }
-        }catch (NumberFormatException e){
-            answer = numString+" can't be fitted anywhere.";
-        }
-        return answer;
-    }
     public static void main(String[] args) {
-        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt() ;
-        sc.nextLine();
-        for(int i=0; i<t;i++){
-            String x = sc.nextLine();
-            System.out.println(findsize(x));
+        int i=1;
+        String s;
+        while (sc.hasNext()) {
+            s=sc.nextLine();
+            System.out.println(i + " " + s);
+            i++;
         }
     }
 }
-
-
-
