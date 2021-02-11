40f458542c8e55199326f0b9ef1dcf70e1ac34c2
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-02-11 20:09:26 +0600

    convert int to string(using Integer)

--- JavaPractice/src/Solution.java
+++ JavaPractice/src/Solution.java
@@ -1,29 +1,49 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
+import java.security.*;
 public class Solution {
- static boolean flag = true;
- static int B,H;
- static{
-     Scanner sc = new Scanner(System.in);
-     B = sc.nextInt();
-     H = sc.nextInt(); 
-     if(B<=0 || H<=0){
-        flag = false;
-        System.out.println("java.lang.Exception: Breadth and height must be positive");
-     }
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+
+   //Write your code here
+   Integer x = new Integer(n);
+   String s= x.toString();
+
+   
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+  } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+  }
  }
- 
-public static void main(String[] args){
-		if(flag){
-			int area=B*H;
-			System.out.print(area);
-		}
-		
-	}//end of main
+}
 
-}//end of class
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
 
+ public static class ExitTrappedException extends SecurityException {
+
+  private static final long serialVersionUID = 1;
+ }
+
+ public static void forbidExit() {
+  final SecurityManager securityManager = new SecurityManager() {
+   @Override
+   public void checkPermission(Permission permission) {
+    if (permission.getName().contains("exitVM")) {
+     throw new ExitTrappedException();
+    }
+   }
+  };
+  System.setSecurityManager(securityManager);
+ }
+}
