7c85b8e703d15e9808be0299b5e54e7ff3601b0a
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-02-27 00:50:02 +0600

    call method from super class

--- JavaPractice/src/Solution.java
+++ JavaPractice/src/Solution.java
@@ -1,26 +1,27 @@
 import java.util.*;
-import java.text.*;
+import java.io.*;
 
-public class Solution {
-    
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double payment = scanner.nextDouble();
-        scanner.close();
+class BiCycle{
+	String define_me(){
+		return "a vehicle with pedals.";
+	}
+}
+
+class MotorCycle extends BiCycle{
+	String define_me(){
+		return "a cycle with an engine.";
+	}
+	
+	MotorCycle(){
         
-        // Write your code here.
-        NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
-        String us = n.format(payment);
-        NumberFormat n1 = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
-        String india = n1.format(payment);
-        NumberFormat n2 = NumberFormat.getCurrencyInstance(Locale.CHINA);
-        String china = n2.format(payment);
-        NumberFormat n3 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-        String france = n3.format(payment);
-        
-        System.out.println("US: " + us);
-        System.out.println("India: " + india);
-        System.out.println("China: " + china);
-        System.out.println("France: " + france);
-    }
+		System.out.println("Hello I am a motorcycle, I am "+ define_me());
+		String temp=super.define_me(); //Fix this line
+		System.out.println("My ancestor is a cycle who is "+ temp );
+	}
+	
+}
+class Solution{
+	public static void main(String []args){
+		MotorCycle M =new MotorCycle();
+	}
 }
---
+++ JavaPractice/src/define_me.java
@@ -0,0 +1,5 @@
+
+
+public interface define_me {
+
+}
